a9274de693d0c8c3ab037b29e422bae69c5cff2b
FoundationGames <43485105+FoundationGames@users.noreply.github.com>
2022-03-26 13:37:11 -0700

    Make necessary baked model attributes serializable

--- src/main/java/foundationgames/enhancedblockentities/client/model/ModelSelector.java
+++ src/main/java/foundationgames/enhancedblockentities/client/model/ModelSelector.java
@@ -8,30 +8,51 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
 
-public interface ModelSelector {
-    ModelSelector STATE_HOLDER_SELECTOR = (view, state, pos, rand, ctx) -> {
-        if(view.getBlockEntity(pos) instanceof ModelStateHolder stateHolder) {
-            return stateHolder.getModelState();
+public abstract class ModelSelector {
+    private static final List<ModelSelector> REGISTRY = new ArrayList<>();
+
+    ModelSelector STATE_HOLDER_SELECTOR = new ModelSelector() {
+        @Override
+        public int getModelIndex(BlockRenderView view, BlockState state, BlockPos pos, Supplier<Random> rand, @Nullable RenderContext ctx) {
+            if(view.getBlockEntity(pos) instanceof ModelStateHolder stateHolder) {
+                return stateHolder.getModelState();
+            }
+            return 0;
         }
-        return 0;
     };
 
     ModelSelector CHEST = STATE_HOLDER_SELECTOR;
 
-    ModelSelector CHEST_WITH_CHRISTMAS = (view, state, pos, rand, ctx) -> {
-        int os = DateUtil.isChristmas() ? 2 : 0;
-        if(view.getBlockEntity(pos) instanceof ModelStateHolder stateHolder) {
-            return stateHolder.getModelState() + os;
+    ModelSelector CHEST_WITH_CHRISTMAS = new ModelSelector() {
+        @Override
+        public int getModelIndex(BlockRenderView view, BlockState state, BlockPos pos, Supplier<Random> rand, @Nullable RenderContext ctx) {
+            int os = DateUtil.isChristmas() ? 2 : 0;
+            if(view.getBlockEntity(pos) instanceof ModelStateHolder stateHolder) {
+                return stateHolder.getModelState() + os;
+            }
+            return os;
         }
-        return os;
     };
 
     ModelSelector BELL = STATE_HOLDER_SELECTOR;
 
     ModelSelector SHULKER_BOX = STATE_HOLDER_SELECTOR;
 
-    int getModelIndex(BlockRenderView view, BlockState state, BlockPos pos, Supplier<Random> rand, @Nullable RenderContext ctx);
+    public abstract int getModelIndex(BlockRenderView view, BlockState state, BlockPos pos, Supplier<Random> rand, @Nullable RenderContext ctx);
+
+    public final int id;
+
+    public ModelSelector() {
+        this.id = REGISTRY.size();
+        REGISTRY.add(this);
+    }
+
+    public static ModelSelector fromId(int id) {
+        return REGISTRY.get(id);
+    }
 }
